979a1b62b790a5e56b922bb0c73fb2a3680a84a6
Josue <yoshuah809@gmail.com>
2022-09-15 19:08:18 -0500

    finished styling the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,62 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+export const NavBrand = styled(Link)`
+	flex-grow: 1;
+	font-size: 28px;
+	font-weight: 700;
+	color: #fff;
+	text-decoration: none;
+`;
+
+export const StyledNavbar = styled.nav`
+	display: flex;
+	justify-content: center;
+	padding: 20px;
+	margin-bottom: 40px;
+
+	@media (max-width: 786px) {
+		flex-direction: column;
+	}
+`;
+export const NavItems = styled.ul`
+	list-style: none;
+	text-decoration: none;
+	display: flex;
+	padding: 0 10%;
+	align-items: center;
+	justify-content: center;
+`;
+export const NavItem = styled(Link)`
+	cursor: pointer;
+	transition: 250ms;
+	cursor: pointer;
+	font-weight: 700;
+	font-size: 24px;
+	color: #fff;
+	text-decoration: none;
+	display: flex;
+	padding: 5px 10px 5px;
+
+	&:hover {
+		border: 1px solid #fff;
+		border-radius: 0.35rem;
+		box-shadow: 1px 3px #fff;
+	}
+`;
 
 const Navbar = () => {
 	return (
 		<>
-			<nav>
-				<div className="nav-center">
-					<h3>blog </h3>
-					<div className="nav-container"></div>
-				</div>
-			</nav>
+			{/*Navigation */}
+			<StyledNavbar>
+				<NavItems>
+					<NavItem to="/">Blog</NavItem>
+					<NavItem to="/about">About</NavItem>
+					<NavItem to="/article-list">Article List</NavItem>
+				</NavItems>
+			</StyledNavbar>
 			<Outlet />
 		</>
 	);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-	return <div>This is the HomePage</div>;
+	return (
+		<div style={{ display: "flex", justifyContent: "center" }}>
+			<h2>This is the HomePage</h2>
+		</div>
+	);
 };
 
 export default HomePage;
